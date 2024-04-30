5ead64d3f4ffc50a55181c4d378ba8c1b1a502f7
Sora <sora0613.contact@gmail.com>
2024-04-30 20:56:16 +0900

    upd. apiの読み込み待ちの際にloading画面を追加。

--- src/src/components/Spinner.tsx
+++ src/src/components/Spinner.tsx
@@ -1,8 +1,15 @@
 // Spinner.tsx
 import React from "react";
+import "../styles/Spinner.css";
 
 const Spinner = () => {
-  return <div className="spinner">{/* Loading spinner */}</div>;
+  return (
+    <div className="spinner">
+      <svg width="120" height="120" viewBox="-60 -60 120 120">
+        <circle r="50" />
+      </svg>
+    </div>
+  );
 };
 
 export default Spinner;
