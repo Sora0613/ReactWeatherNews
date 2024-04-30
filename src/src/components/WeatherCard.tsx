56fcece27f130b7958d283ff27766e7d9e265fd8
Sora <sora0613.contact@gmail.com>
2024-04-30 23:16:42 +0900

    upd. 天気表示画面のデザインと仕組みを少し修正。 改善の余地あり。

--- src/src/components/WeatherCard.tsx
+++ src/src/components/WeatherCard.tsx
@@ -1,94 +1,122 @@
-// WeatherCard.tsx
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faSun,
-  faCloud,
-  faCloudSun,
-  faCloudRain,
-  faWind,
+    faSun,
+    faCloud,
+    faCloudSun,
+    faCloudRain,
+    faWind, IconDefinition,
 } from "@fortawesome/free-solid-svg-icons";
 import Spinner from "./Spinner";
 
 interface WeatherCardProps {
-  cityName: string;
+    cityName: string;
 }
 
 function getDate() {
-  const today = new Date();
-  const month = today.getMonth() + 1;
-  const year = today.getFullYear();
-  const date = today.getDate();
-  return `${year}年${month}月${date}日`;
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${year}年${month}月${date}日`;
 }
 
-const WeatherCard = (props: any) => {
-  const [currentDate, setCurrentDate] = useState(getDate());
-  const [data, setData] = useState<any>();
+function getWeatherIcon(DayWeather: String) {
+    let TheWeatherIcon;
+    if (DayWeather.includes("晴")) {
+        TheWeatherIcon = <FontAwesomeIcon icon={faSun} size="5x"/>;
+    } else if (DayWeather.includes("雨")) {
+        TheWeatherIcon = <FontAwesomeIcon icon={faCloudRain} size="5x"/>;
+    } else if (DayWeather.includes("くもり")) {
+        TheWeatherIcon = <FontAwesomeIcon icon={faCloud} size="5x"/>;
+    } else {
+        TheWeatherIcon = <FontAwesomeIcon icon={faCloudSun} size="5x"/>;
+    }
+    return TheWeatherIcon;
+}
 
-  useEffect(() => {
-    fetch(
-      "https://www.jma.go.jp/bosai/forecast/data/forecast/" +
-        props.CityCode +
-        ".json",
-    )
-      .then((res) => res.json())
-      .then((json) => setData(json))
-      .catch(() => console.log("error"));
-  }, []);
+const WeatherCard = (props: WeatherCardProps) => {
+    const [currentDate, setCurrentDate] = useState(getDate());
+    const [data, setData] = useState<any>();
 
-  console.log(data);
+    useEffect(() => {
+        fetch(
+            "https://www.jma.go.jp/bosai/forecast/data/forecast/" +
+            props.cityName +
+            ".json",
+        )
+            .then((res) => res.json())
+            .then((json) => setData(json))
+            .catch(() => console.log("error"));
+    }, [props.cityName]);
 
-  if (!data) {
+    console.log(data);
+
+    if (!data) {
+        return (
+            <Spinner/>
+        );
+    }
+
+    const place = data[0].timeSeries[0].areas[0].area.name;
+    const today_Weather = data[0].timeSeries[0].areas[0].weathers[0];
+    const tomorrow_weather = data[0].timeSeries[0].areas[0].weathers[1];
+    const the_day_after_tomorrow_weather =
+        data[0].timeSeries[0].areas[0].weathers[2];
+    const min_temp = data[1].tempAverage.areas[0].min;
+    const max_temp = data[1].tempAverage.areas[0].max;
+
+
+    let todayWeatherIcon = getWeatherIcon(today_Weather);
+    let tomorrowWeatherIcon = getWeatherIcon(tomorrow_weather);
+    let dayAfterTomorrowWeatherIcon = getWeatherIcon(the_day_after_tomorrow_weather);
+
     return (
-      <div className="weather-card-container">
-        <div className="weather-info">
-          <div className="weather-header">
-            <Spinner />
-            <p>Now Loading...</p>
-          </div>
+        <div className="weather-card-container">
+            <div className="weather-info">
+                <div className="weather-header">
+                    <h2>{place}の天気</h2>
+                    <p>{currentDate}</p>
+                </div>
+                <div className="weather-details">
+                    <div className="weather-today">
+                        <div className="weather-icon">
+                            {todayWeatherIcon}
+                        </div>
+                        <div className="weather-description">
+                            <p className="weather-title">今日の{place}の天気：</p>
+                            <p>{today_Weather}</p>
+                            <p className="weather-temp-high">最高気温: <span className="red">{max_temp}°C</span></p>
+                            <p className="weather-temp-low">最低気温: <span className="blue">{min_temp}°C</span></p>
+                        </div>
+                    </div>
+                    <hr className="weather-divider"/>
+                    <div className="weather-tomorrow">
+                        <div className="weather-icon">
+                            {tomorrowWeatherIcon}
+                        </div>
+                        <div className="weather-description">
+                            <p className="weather-title">明日の{place}の天気：</p>
+                            <p>{tomorrow_weather}</p>
+                        </div>
+                    </div>
+                    <hr className="weather-divider"/>
+                    <div className="weather-day-after-tomorrow">
+                        <div className="weather-icon">
+                            {dayAfterTomorrowWeatherIcon}
+                        </div>
+                        <div className="weather-description">
+                            <p className="weather-title">明後日の{place}の天気：</p>
+                            <p>{the_day_after_tomorrow_weather}</p>
+                        </div>
+                    </div>
+                    <hr className="weather-divider"/>
+                </div>
+            </div>
         </div>
-      </div>
+
+
     );
-  }
-
-  const place = data[0].timeSeries[0].areas[0].area.name;
-  const today_Weather = data[0].timeSeries[0].areas[0].weathers[0];
-  const tomorrow_weather = data[0].timeSeries[0].areas[0].weathers[1];
-  const the_day_after_tomorrow_weather =
-    data[0].timeSeries[0].areas[0].weathers[2];
-  const min_temp = data[1].tempAverage.areas[0].min;
-  const max_temp = data[1].tempAverage.areas[0].max;
-
-  return (
-    <div className="weather-card-container">
-      <div className="weather-info">
-        <div className="weather-header">
-          <h2>{place}の天気</h2>
-          <p>{currentDate}</p>
-        </div>
-        <div className="weather-icon">
-          <FontAwesomeIcon icon={faSun} size="5x" />
-        </div>
-        <div className="weather-details">
-          <p>
-            今日の{place}の天気：{today_Weather}
-          </p>
-          <p>
-            明日の{place}の天気：{tomorrow_weather}
-          </p>
-          <p>
-            明後日の{place}の天気：{the_day_after_tomorrow_weather}
-          </p>
-          <p>最低気温: {min_temp}°C</p>
-          <p>最高気温: {max_temp}°C</p>
-        </div>
-        <div className="weather-footer">
-          <p>2024 &copy; Sora</p>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default WeatherCard;
