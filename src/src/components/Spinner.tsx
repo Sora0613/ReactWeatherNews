bfdd5f5651800e0a9b20ba506a10984a20b82cb1
Sora <sora0613.contact@gmail.com>
2024-04-30 23:16:15 +0900

    upd. Spinnerをコンポーネントで整理。

--- src/src/components/Spinner.tsx
+++ src/src/components/Spinner.tsx
@@ -3,13 +3,21 @@ import React from "react";
 import "../styles/Spinner.css";
 
 const Spinner = () => {
-  return (
-    <div className="spinner">
-      <svg width="120" height="120" viewBox="-60 -60 120 120">
-        <circle r="50" />
-      </svg>
-    </div>
-  );
+    return (
+        <div className="weather-card-container">
+            <div className="weather-info">
+                <div className="weather-header">
+                    <div className="spinner">
+                        <svg width="120" height="120" viewBox="-60 -60 120 120">
+                            <circle r="50"/>
+                        </svg>
+                    </div>
+                    <p>Now Loading...</p>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default Spinner;
