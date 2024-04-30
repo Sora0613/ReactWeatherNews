d75841cbc56240460cb1d9e06ad555c935c9aea4
Sora <sora0613.contact@gmail.com>
2024-04-30 23:16:00 +0900

    upd. 検索バーを動くように

--- src/src/components/SearchBar.tsx
+++ src/src/components/SearchBar.tsx
@@ -1,13 +1,33 @@
-// SearchBar.tsx
-import React from "react";
+import React, {useState} from "react";
 
-const SearchBar = () => {
-  return (
-    <div className="search-bar">
-      <input type="text" placeholder="都市を入力してください" />
-      <button>検索</button>
-    </div>
-  );
+interface SearchBarProps {
+    onSearch: (cityName: string) => void;
+}
+
+const SearchBar = (props: SearchBarProps) => {
+    const [cityName, setCityName] = useState("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCityName(e.target.value);
+    };
+
+    const handleClick = () => {
+        props.onSearch(cityName);
+
+        setCityName("");
+    };
+
+    return (
+        <div className="search-bar">
+            <input
+                type="text"
+                placeholder="都市名"
+                value={cityName}
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}>検索</button>
+        </div>
+    );
 };
 
 export default SearchBar;
