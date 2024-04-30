3e1d7d17d42d13c32be1aec5eb70b0a11f7ee1ec
Sora <sora0613.contact@gmail.com>
2024-04-30 23:15:11 +0900

    upd. 検索バーを動くように。

--- src/src/pages/HomePage.tsx
+++ src/src/pages/HomePage.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import WeatherCard from "../components/WeatherCard";
 import SearchBar from "../components/SearchBar";
 import LocationButton from "../components/LocationButton";
+import FetchCityCode from "../components/FetchCityCode";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
-  return (
-    <div className="home-page">
-      <h1>Weather Forecast</h1>
-      <SearchBar />
-      <LocationButton />
-      <WeatherCard CityCode="270000" />
-    </div>
-  );
+    const [cityCode, setCityCode] = useState("130000");
+
+    const handleSearch = (cityName: string) => {
+        const code = FetchCityCode({ cityName });
+        if (code) {
+            setCityCode(code);
+        } else {
+            alert("Error: 街が見つかりませんでした。");
+        }
+    };
+
+    return (
+        <div className="home-page">
+            <h1>天気予報</h1>
+            <SearchBar onSearch={handleSearch} />
+            <LocationButton />
+            {cityCode && <WeatherCard cityName={cityCode} />}
+            <Footer />
+        </div>
+    );
 };
 
 export default HomePage;
-
-// CityCodeに関しては初期値を東京にし、現在地からCityCodeを割り出して出力するようにしたい。
