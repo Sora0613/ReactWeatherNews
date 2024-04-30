60d31a3e20c9c5de5417a74dc4eb59f1ec3124e7
Sora <sora0613.contact@gmail.com>
2024-04-30 23:12:51 +0900

    add. このサイトについてを追加。

---
+++ src/src/pages/AboutSitePage.tsx
@@ -0,0 +1,14 @@
+// AboutSitePage.tsx
+import React from "react";
+
+const AboutSitePage = () => {
+    return (
+        <div>
+            <h2>About This Site</h2>
+            <p>This is a weather forecast application created by Sora.</p>
+            {/* 追加の情報やコンテンツをここに追加 */}
+        </div>
+    );
+};
+
+export default AboutSitePage;
