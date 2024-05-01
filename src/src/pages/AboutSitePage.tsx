31bb9cfa448f8defca0f16513751f59336388ea2
Sora <sora0613.contact@gmail.com>
2024-05-01 17:35:43 +0900

    upd. 簡易的なサイトの紹介を追加。

--- src/src/pages/AboutSitePage.tsx
+++ src/src/pages/AboutSitePage.tsx
@@ -3,10 +3,13 @@ import React from "react";
 
 const AboutSitePage = () => {
     return (
-        <div>
-            <h2>About This Site</h2>
-            <p>This is a weather forecast application created by Sora.</p>
-            {/* 追加の情報やコンテンツをここに追加 */}
+        <div className="home-page">
+            <div className="weather-card-container">
+                <h2>About This Site</h2>
+                <p>各都道府県の今日、明日、明後日の天気を一目で確認することができるWebサイトです。</p>
+                <p>Reactで作りました。</p>
+                <p>APIは気象庁のAPIを使っています。</p>
+            </div>
         </div>
     );
 };
