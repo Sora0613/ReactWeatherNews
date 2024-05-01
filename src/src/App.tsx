5ee200c92de77690eea48eb9ce4978b29f72dd0d
Sora <sora0613.contact@gmail.com>
2024-05-01 17:36:17 +0900

    upd. DarkMode追加に伴う修正

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutSitePage from "./pages/AboutSitePage";
+import {DarkModeProvider} from "./components/DarkModeContent";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about-site" element={<AboutSitePage />} />
-            </Routes>
-        </BrowserRouter>
+        <DarkModeProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/about-site" element={<AboutSitePage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </DarkModeProvider>
     );
 };
 
