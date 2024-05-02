e39742206d2c8790e66b380b04c45a6d5942a133
Sora <sora0613.contact@gmail.com>
2024-05-02 23:45:18 +0900

    upd. 使用するAPIの変更を行なったので検索方法の変更を行なった。

--- src/src/pages/HomePage.tsx
+++ src/src/pages/HomePage.tsx
@@ -1,22 +1,18 @@
 import React, {useState} from "react";
 import WeatherCard from "../components/WeatherCard";
 import SearchBar from "../components/SearchBar";
-import FetchCityCode from "../components/FetchCityCode";
 import Footer from "../components/Footer";
 import ToggleSwitch from "../components/ToggleSwitch";
 import {useDarkMode} from "../components/DarkModeContent";
 
 const HomePage = () => {
-    const [cityCode, setCityCode] = useState("130000");
+    const [cityName, setCityName] = useState("東京");
     const {isDarkMode, toggleDarkMode} = useDarkMode();
 
     const handleSearch = (cityName: string) => {
-        const code = FetchCityCode({cityName});
-        if (code) {
-            setCityCode(code);
-        } else {
-            alert("Error: 街が見つかりませんでした。");
-        }
+        // translate the city name (jpn) to roma-ji
+
+        setCityName(cityName);
     };
 
     return (
@@ -25,7 +21,7 @@ const HomePage = () => {
                 <h1>天気予報</h1>
                 <ToggleSwitch/>
                 <SearchBar onSearch={handleSearch}/>
-                {cityCode && <WeatherCard cityName={cityCode}/>}
+                {cityName && <WeatherCard cityName={cityName}/>}
                 <Footer/>
             </div>
         </div>
