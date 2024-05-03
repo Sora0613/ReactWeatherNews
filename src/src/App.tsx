f22bee2128956aa405a0ed6e658091e9590af96b
Sora <sora0613.contact@gmail.com>
2024-05-03 15:15:24 +0900

    upd. APIの変更にともない、都市に関する詳細情報を記載するルートの作成。

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutSitePage from "./pages/AboutSitePage";
-import {DarkModeProvider} from "./components/DarkModeContent";
+import CityPage from "./pages/CityPage";
+import { DarkModeProvider } from "./components/DarkModeContent";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
                     <Route path="/about-site" element={<AboutSitePage/>}/>
+                    <Route path="/:city" element={<CityPage />}/>
                 </Routes>
             </BrowserRouter>
         </DarkModeProvider>
