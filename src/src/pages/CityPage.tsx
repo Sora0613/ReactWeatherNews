3aa18bfc103e25d11588c3cdc8be9ca342bbc0d8
Sora <sora0613.contact@gmail.com>
2024-05-08 15:21:25 +0900

    upd. UnixTimeを日本時間に変換する関数の追加。 当日の天気の取得のみだったのを明日以降の天気も追加。

--- src/src/pages/CityPage.tsx
+++ src/src/pages/CityPage.tsx
@@ -7,6 +7,12 @@ import {faSun} from "@fortawesome/free-solid-svg-icons";
 import TranslateJapanese from "../components/TranslateJapanese";
 import WeatherIcon from "../components/WeatherIcon";
 
+function setDate(unixtime: number) {
+    const date = new Date(unixtime * 1000);
+    return date.toLocaleTimeString([], { month:'2-digit', day: '2-digit' ,hour: '2-digit' });
+
+}
+
 const CityPage = () => {
     const appid = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
     const {city} = useParams<{ city: string }>();
@@ -17,6 +23,7 @@ const CityPage = () => {
     const forecast_endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${appid}&lang=${lang}&units=${units}`;
 
     const forecast_data = FetchData({endpoint: forecast_endpoint, city});
+
     if (!forecast_data) {
         return (
             <Spinner/>
@@ -32,8 +39,30 @@ const CityPage = () => {
     const todayWeatherMinTemp = todayWeather.main.temp_min;
 
     // 日付系。
-    const currentDate = new Date().toLocaleDateString();
-    const time = new Date(todayWeather.dt * 1000).toLocaleTimeString();
+    const time = setDate(todayWeather.dt);
+
+    // 明日以降の天気
+    const futureWeather = forecast_data.list.slice(1);
+    const futureWeatherElements = futureWeather.map((weather: any, index: any) => (
+        <>
+            <div key={index} className="weather-today">
+                <div className="weather-icon">
+                    <WeatherIcon icon={weather.weather[0].icon}/>
+                </div>
+                <div className="weather-description">
+                    <p>{setDate(weather.dt)}の天気</p>
+                    <p>{weather.weather[0].description}</p>
+                    <p>湿度：{weather.main.humidity}%</p>
+                    <p className="weather-temp-high">最高気温: <span className="red">{weather.main.temp_max}°C</span>
+                    </p>
+                    <p className="weather-temp-low">最低気温: <span className="blue">{weather.main.temp_min}°C</span>
+                    </p>
+                </div>
+            </div>
+            <hr className="weather-divider"/>
+        </>
+
+    ));
 
 
     return (
@@ -46,18 +75,21 @@ const CityPage = () => {
                             <WeatherIcon icon={todayWeatherIcon}/>
                         </div>
                         <div className="weather-description">
-                            <p className="weather-title">{currentDate} {time}の{place}の天気：</p>
+                            <p className="weather-title">{time}の{place}の天気：</p>
                             <TranslateJapanese icon={todayWeatherIcon}/>
                             <p>湿度：{todayWeatherHumidity}%</p>
-                            <p className="weather-temp-high">最高気温: <span className="red">{todayWeatherMaxTemp}°C</span>
+                            <p className="weather-temp-high">最高気温: <span
+                                className="red">{todayWeatherMaxTemp}°C</span>
                             </p>
-                            <p className="weather-temp-low">最低気温: <span className="blue">{todayWeatherMinTemp}°C</span>
+                            <p className="weather-temp-low">最低気温: <span
+                                className="blue">{todayWeatherMinTemp}°C</span>
                             </p>
                         </div>
                     </div>
-                    TODO:ここから明日以降の天気を表示する。JSONから取得できる限りを取得して表示させたい。
                     <hr className="weather-divider"/>
-
+                    <div className="weather-forecast-container">
+                        {futureWeatherElements}
+                    </div>
                     <Link to="/">トップへ戻る</Link>
                 </div>
             </div>
