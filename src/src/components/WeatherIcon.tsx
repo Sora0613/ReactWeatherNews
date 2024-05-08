e8165c8febd77a20118f785d4958eff4036b56ba
Sora <sora0613.contact@gmail.com>
2024-05-08 15:24:57 +0900

    upd. 夜のアイコンがエラーを出していたので対応させた。

--- src/src/components/WeatherIcon.tsx
+++ src/src/components/WeatherIcon.tsx
@@ -21,34 +21,43 @@ function getWeatherIcon(DayWeatherIcon: string) {
 
     switch (DayWeatherIcon) {
         case "01d":
+        case "01n":
             TheWeatherIcon = <FontAwesomeIcon icon={faSun} size="5x"/>;
             break;
         case "02d":
+        case "02n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloudSun} size="5x"/>;
             break;
         case "03d":
+        case "03n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloud} size="5x"/>;
             break;
         case "04d":
+        case "04n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloud} size="5x"/>;
             break;
         case "09d":
+        case "09n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloudRain} size="5x"/>;
             break;
         case "10d":
+        case "10n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} size="5x"/>;
             break;
         case "11d":
+        case "11n":
             TheWeatherIcon = <FontAwesomeIcon icon={faCloudBolt} size="5x"/>;
             break;
         case "13d":
+        case "13n":
             TheWeatherIcon = <FontAwesomeIcon icon={faSnowflake} size="5x"/>;
             break;
         case "50d":
+        case "50n":
             TheWeatherIcon = <FontAwesomeIcon icon={faSmog} size="5x"/>;
             break;
         default:
-            TheWeatherIcon = <FontAwesomeIcon icon={faQuestion} size="5x"/>;
+            TheWeatherIcon = <img src={`https://openweathermap.org/img/wn/${DayWeatherIcon}@2x.png`} alt="weather-icon"/>;
             break;
     }
     return TheWeatherIcon;
